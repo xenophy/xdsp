55fdf9b1f23652b8f403f350bc73f00e3da24f7b
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2022-07-07 15:23:54 +0900

    add update_list.yml generator

---
+++ generate.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+PLAYBOOKS_DIR="playbooks"
+TARGET="${SCRIPT_DIR}/${PLAYBOOKS_DIR}/*"
+BASE_URL="https://xenophy.github.io/xdsp/playbooks"
+
+echo "playbooks:" > "$SCRIPT_DIR"/update_list.yml
+
+for f in $TARGET; do
+
+  BASENAME=`basename $f`
+  NAME=`echo $BASENAME | sed 's/\.[^\.]*$//'`
+
+  echo "file: $f"
+  echo "basename: $BASENAME"
+  echo "  $NAME: $BASE_URL/$BASENAME" >> "$SCRIPT_DIR"/update_list.yml
+
+done
